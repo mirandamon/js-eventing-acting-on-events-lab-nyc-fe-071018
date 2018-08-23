e39dd1cba21bb4b30261320d442a5751fad3efdf
Nathan Miranda <nate845@gmail.com>
2018-08-23 23:02:10 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+const element = document.getElementById('dodger')
+
+document.addEventListener('keydown', (event) => {
+  if (event.which === 37) {
+    // '180px'
+    let previousLeft = element.style.left
+    // 180
+    let previousLeftNumber = parseInt(previousLeft.replace('px', ''), 10)
+    if (previousLeftNumber > 0) {
+      let newLeftNumber = (previousLeftNumber - 15) + "px"
+      console.log(newLeftNumber)
+      element.style.left = newLeftNumber
+    }
+  }
+})
+
+function moveDodgerRight() {
+  document.addEventListener('keydown', (event) => {
+    if (event.which === 39) {
+      let previousRight = element.style.left
+      // 180
+      let previousRightNumber = parseInt(previousRight.replace('px', ''), 10)
+      if (previousRightNumber < 360) {
+        let newRightNumber = (previousRightNumber + 15) + "px"
+        console.log(newRightNumber)
+        element.style.left = newRightNumber
+      }
+    }
+  })
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  moveDodgerRight()
+})
